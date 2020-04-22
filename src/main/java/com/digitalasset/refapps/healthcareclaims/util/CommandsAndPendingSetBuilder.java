8c5a1901bb76c48ff64325ef80db4945631b64d8
Gabor Hosszu <gabor@digitalasset.com>
2020-04-22 13:29:08 +0200

    Fix formatter

--- src/main/java/com/digitalasset/refapps/healthcareclaims/util/CommandsAndPendingSetBuilder.java
+++ src/main/java/com/digitalasset/refapps/healthcareclaims/util/CommandsAndPendingSetBuilder.java
@@ -88,7 +88,14 @@ public class CommandsAndPendingSetBuilder {
         Instant mrt = now.plus(mrtDuration);
         SubmitCommandsRequest commandsRequest =
             new SubmitCommandsRequest(
-                workflowId, appId, UUID.randomUUID().toString(), party, Optional.of(mrt), Optional.empty(), Optional.empty(), commands);
+                workflowId,
+                appId,
+                UUID.randomUUID().toString(),
+                party,
+                Optional.of(mrt),
+                Optional.empty(),
+                Optional.empty(),
+                commands);
         return Optional.of(
             new CommandsAndPendingSet(commandsRequest, HashTreePMap.from(pendingContractIds)));
       }
