98d58609245c12b1f363443d3ee81a998f2f68fe
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2021-01-13 10:56:26 +0100

    Added missing license header.

--- scripts/create-ledger-setup.js
+++ scripts/create-ledger-setup.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 const files = require('fs');
 
 const [participantsFile, ledgerSetupConfigFile] = process.argv.slice(2)
